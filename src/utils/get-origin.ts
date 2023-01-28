069f00b603fb3dd453fc723e66ce268904935e05
George Wilson <wilson.george2603@gmail.com>
2023-01-28 14:40:34 +0000

    ci: add vercel url

--- src/utils/get-origin.ts
+++ src/utils/get-origin.ts
@@ -1,6 +1,6 @@
 export function getOrigin() {
   if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
   }
 
   return `http://localhost:${process.env.PORT ?? 3000}`;
